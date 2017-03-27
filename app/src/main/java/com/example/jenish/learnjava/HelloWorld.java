f6c6ca6dd7325cd9d16144d492d5ace505701d07
jenishpat <jenishpat@gmail.com>
2017-03-26 21:26:47 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jenish/learnjava/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.jenish.learnjava;
+
+/**
+ * Created by Jenish on 2017-03-26.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
