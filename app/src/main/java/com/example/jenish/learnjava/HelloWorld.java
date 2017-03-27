939a3dc4340539d982fb2daa27c2c75223796151
jenishpat <jenishpat@gmail.com>
2017-03-26 22:57:07 -0400

    Initial commit

--- app/src/main/java/com/example/jenish/learnjava/HelloWorld.java
+++ app/src/main/java/com/example/jenish/learnjava/HelloWorld.java
@@ -7,5 +7,8 @@ package com.example.jenish.learnjava;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        String jenish = "Jenish Patel";
+        System.out.println(jenish);
     }
 }
