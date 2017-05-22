c5070591eb35b1bed840eb8795f30d824f620981
jenishpat <jenishpat@gmail.com>
2017-05-22 01:40:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jenish/learnjava/Demo.java
@@ -0,0 +1,32 @@
+package com.example.jenish.learnjava;
+
+/**
+ * Created by Jenish on 2017-05-09.
+ */
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Player jenish = new Player("Jenish Patel"); // Created instance of a player class
+       /* System.out.println("Player: " + jenish.getHandleName());
+        System.out.println("Level: " + jenish.getLevel());
+        System.out.println("Lives: " + jenish.getLives());
+        System.out.println("Weapon: " + jenish.getWeapon().getName());*/
+
+        Weapon myAxe = new Weapon("Goldshine Axe", 20, 50);
+        /*jenish.setWeapon(myAxe);
+        System.out.println(jenish.getWeapon().getName());*/
+
+        Loot redPotion = new Loot("Red Potion", LootType.POTION, 7);
+        jenish.pickUpLoot(redPotion);
+        jenish.pickUpLoot(new Loot("+3 Chest Armor", LootType.ARMOR, 80));
+        jenish.pickUpLoot(new Loot("Ring of Protection +2", LootType.RING, 40));
+        jenish.pickUpLoot(new Loot("Invisibility Potion", LootType.POTION, 35));
+
+        jenish.showInventory();
+
+        boolean wasDeleted = jenish.dropLoot(redPotion);
+        System.out.println(wasDeleted);
+        jenish.showInventory();
+    }
+}
--- app/src/main/java/com/example/jenish/learnjava/Player.java
+++ app/src/main/java/com/example/jenish/learnjava/Player.java
@@ -1,8 +1,126 @@
 package com.example.jenish.learnjava;
 
+import java.util.ArrayList;
+
 /**
  * Created by Jenish on 2017-05-06.
  */
 
-public class Player  {
+public class Player {
+    private String handleName;
+    private int lives;
+    private int level;
+    private int score;
+    private Weapon weapon;
+    private ArrayList<Loot> inventory;
+
+    // Constructor
+    public Player() {
+        this("Unknown player");
+    }
+
+    // Method Overloading
+    public Player(String handle) {
+        this(handle, 1);
+    }
+
+    // Method Overloading
+    public Player(String handle, int startingLevel) {
+//        this.handleName = handle;
+//        this.level = startingLevel;
+//        this.lives = 3;
+//        this.score = 0;
+        setHandleName(handle);
+        setLevel(startingLevel);
+        setLives(3);
+        setScore(0);
+//        setDefaultWeapon();
+        inventory = new ArrayList<>();
+    }
+
+    // **Getter and Setter**
+    public String getHandleName() {
+        return handleName;
+    }
+
+    public void setHandleName(String handle) {
+        if (handle.length() < 3) {
+            System.out.println("The name " + handle + " is too short, must be 3 characters or more.");
+            return;
+        }
+        this.handleName = handle;
+    }
+
+    private void setDefaultWeapon() {
+        this.weapon = new Weapon("Sword", 10, 20);
+    }
+
+    public void setNameAndLevel(String name, int Level) {
+        setHandleName(name);
+        setLevel(Level);
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public ArrayList<Loot> getInventory() {
+        return inventory;
+    }
+
+    /*    public void setInventory(ArrayList<Loot> inventory) {
+            this.inventory = inventory;
+        }*/
+
+    // Adding items into the inventory
+    public void pickUpLoot(Loot newLoot) {
+        inventory.add(newLoot);
+    }
+
+    // Dropping items from the inventory
+    public boolean dropLoot(Loot loot) {
+        if (this.inventory.contains(loot)) {
+            inventory.remove(loot);
+            return true;
+        }
+        return false;
+    }
+
+    // Goes through the array and displays every item in the inventory
+    public void showInventory() {
+        for (Loot item : inventory){
+            System.out.println(item.getName());
+        }
+        System.out.println("=========================");
+    }
 }
+
+
